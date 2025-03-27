6a2f10e9f5e81b551e650d5251b3bf893ccad870
Rustam <apaloget74@gmaail.com>
2025-03-27 17:02:02 +0100

    Initial Commit

---
+++ PhysicsCalculator/PhysicsCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhysicsCalculatorApp.swift
+//  PhysicsCalculator
+//
+//  Created by rustam on 27.03.25.
+//
+
+import SwiftUI
+
+@main
+struct PhysicsCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
